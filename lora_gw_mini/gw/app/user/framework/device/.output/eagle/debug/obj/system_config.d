2e1eb3f386a9d2b43157d53bdd5986717266685e
hehao <hehao@126.com>
2018-11-10 11:15:56 +0800

    rm nonos V2.00

--- lora_gw_mini/gw/app/user/framework/device/.output/eagle/debug/obj/system_config.d
+++
@@ -1,5 +0,0 @@
-.output/eagle/debug/obj/system_config.o .output/eagle/debug/obj/system_config.d : system_config.c system_config.h ../core/core.h \
- ../core/msc_types.h ../core/debug.h ../core/mem_mgr.h \
- ../../../../include/os_type.h ../../../../include/ets_sys.h \
- ../../../../include/c_types.h ../../../../include/eagle_soc.h \
- ../core/core.h ../core/msc_list.h ../core/protocol.h
