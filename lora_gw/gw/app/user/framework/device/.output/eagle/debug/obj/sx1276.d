b5ce5646e45b32216458b685a435d8619ba1234d
unknown <052646180@hdjt.hdad.local>
2019-07-31 11:01:35 +0800

    rm lora gw

--- lora_gw/gw/app/user/framework/device/.output/eagle/debug/obj/sx1276.d
+++
@@ -1,5 +0,0 @@
-.output/eagle/debug/obj/sx1276.o .output/eagle/debug/obj/sx1276.d : sx1276.c sx1276.h ../core/core.h ../core/msc_types.h \
- ../core/debug.h ../core/mem_mgr.h ../../../../include/os_type.h \
- ../../../../include/ets_sys.h ../../../../include/c_types.h \
- ../../../../include/eagle_soc.h ../core/core.h ../core/msc_list.h \
- ../core/protocol.h
--- lora_gw/gw/app/user/framework/device/.output/eagle/debug/obj/sx1276_hal.d
+++
@@ -1,5 +0,0 @@
-.output/eagle/debug/obj/sx1276_hal.o .output/eagle/debug/obj/sx1276_hal.d : sx1276_hal.c sx1276.h ../core/core.h ../core/msc_types.h \
- ../core/debug.h ../core/mem_mgr.h ../../../../include/os_type.h \
- ../../../../include/ets_sys.h ../../../../include/c_types.h \
- ../../../../include/eagle_soc.h ../core/core.h ../core/msc_list.h \
- ../core/protocol.h sx1276_hal.h
